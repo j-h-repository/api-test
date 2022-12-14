fb27adde88bf4cf6c26408bbb9448ca5796fb395
sailoryoung <jordonharris@protonmail.com>
2022-12-14 07:39:47 -0500

    cors 3

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cors(
 	{
-		"origin": "*",
-		"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-		"preflightContinue": false,
-		"optionsSuccessStatus": 204
+		origin: "http://localhost:3000/",
+		
 	  }
 ))
 
