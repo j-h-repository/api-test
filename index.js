77f9d95dedcb89df9252be254143cb55bccd2452
sailoryoung <jordonharris@protonmail.com>
2022-12-14 10:15:29 -0500

    UPDATE ENV

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cors(
 	{
-		origin: "http://localhost:3000/",
+		origin: [process.env.LOCAL_HOST],
 		
 	  }
 ))
